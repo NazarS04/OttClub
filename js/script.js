aaf06575088182af410af723bd21df6fc342b357
Nazar <ns15052004@gmail.com>
2023-08-13 19:43:54 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// ------------------- Channels -------------------
+
 const channelsContainer = document.querySelector(".channels__channels");
 const moreChannelsBtn = document.querySelector(".channels__more-channels");
 
@@ -10,5 +12,29 @@ moreChannelsBtn.addEventListener("click", function (e) {
     channelsContainer.style.maxHeight = channelsContainer.scrollHeight + "px";
   } else {
     channelsContainer.style.maxHeight = "";
+  }
+})
+
+// ------------------- Questions -------------------
+document.querySelector(".popular-questions__questions").addEventListener("click", function (e) {
+  let target = e.target;
+
+  if (target.tagName !== "BUTTON") {
+    target = target.closest(".question__button");
+  }
+
+  if (!target) {
+    return;
+  }
+
+  const question = target.closest(".question");
+  const textWrapper = question.querySelector(".question__text-wrapper");
+
+  question.classList.toggle("question_active");
+
+  if (question.classList.contains("question_active")) {
+    textWrapper.style.height = textWrapper.scrollHeight + "px";
+  } else {
+    textWrapper.style.height = "";
   }
 })
